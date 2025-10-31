bb8e6e1529467949eb7c6f4e9770789c9beb25fb
zeineb maatalli <157981311+zeineb-m@users.noreply.github.com>
2025-10-31 23:55:29 +0100

    Update settings.py

--- CyberCobra/settings.py
+++ CyberCobra/settings.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 # -----------------------------
 
-BASE_DIR = Path(**file**).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # -----------------------------
 
