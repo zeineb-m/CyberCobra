34f7376b7a3003fa70253a36ea0991970909a3b7
zeineb maatalli <157981311+zeineb-m@users.noreply.github.com>
2025-10-31 22:54:06 +0100

    Add redirect for root URL to external site
    Redirect root URL to external site.

--- CyberCobra/urls.py
+++ CyberCobra/urls.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.http import HttpResponseRedirect
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('auth_app.urls')),
     path('report/', include('report.urls')),
     path('api/', include('gestion_dequipement.urls')),
     path('api/', include('gestion_camera.urls')),
-    path('api/', include('zones_app.urls'))
+    path('api/', include('zones_app.urls')),
+ path('', lambda request: HttpResponseRedirect('https://cybercobra-2.onrender.com/')),
 ]
 
 if settings.DEBUG:
